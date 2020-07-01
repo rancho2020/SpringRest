e2d4f4f14d4960e3f385138594bf75e013bd9d7a
Rancho Coder <rancho.coder@gmail.com>
2020-07-01 21:54:16 +0530

    dao added

--- src/main/java/com/rest/controller/MyRestController.java
+++ src/main/java/com/rest/controller/MyRestController.java
@@ -1,10 +1,8 @@
 package com.rest.controller;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.CacheControl;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -27,27 +25,23 @@ public class MyRestController {
 
 	@GetMapping(path = "/get/allData", produces = MediaType.APPLICATION_JSON_VALUE)
 	public List<Billionaires> getText() {
-		System.out.println("Controller getAll");
 		return billionairesService.listAll();
 	}
 
 	@DeleteMapping(path = "/delete/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<String> delete(@PathVariable Long id) {
-		System.out.println("Controller DELETE");
 		billionairesService.deleteBillionaire(id);
 		return ResponseEntity.ok().body("User deleted successfully");
 	}
 
 	@PostMapping(path = "/save/", produces = MediaType.APPLICATION_JSON_VALUE)
 	public String save(@RequestBody Billionaires billionaires) {
-		System.out.println("Controller SAVE");
 		billionairesService.createBillionaires(billionaires);
 		return "User deleted successfully";
 	}
 
 	@PostMapping(path = "/update/", produces = MediaType.APPLICATION_JSON_VALUE)
 	public String update(@RequestBody Billionaires billionaires) {
-		System.out.println("Controller UPDATE");
 		billionairesService.updateBillionaire(billionaires);
 		return "User deleted successfully";
 	}
--- src/main/java/com/rest/service/BillionairesServiceImpl.java
+++ src/main/java/com/rest/service/BillionairesServiceImpl.java
@@ -9,37 +9,28 @@ import com.rest.dao.BillionairesDaoImpl;
 import com.rest.domain.Billionaires;
 
 @Service
-public class BillionairesServiceImpl implements BillionairesService{
-	
+public class BillionairesServiceImpl implements BillionairesService {
+
 	@Autowired
 	private BillionairesDaoImpl billionairesDaoImpl;
 
-    public List<Billionaires> listAll() {
-       System.out.println("SEERVVICE listAll---");
-    	return (List<Billionaires>) billionairesDaoImpl.getBillionaires();
-    	//return list;
-    }
-     
-	 
+	public List<Billionaires> listAll() {
+		return (List<Billionaires>) billionairesDaoImpl.getBillionaires();
+	}
+
 	@Override
 	public int deleteBillionaire(Long bilionaireiD) {
-		System.out.println("SEERVVICE delete---");
-		  return billionairesDaoImpl.deleteBillionaire(bilionaireiD); 
+		return billionairesDaoImpl.deleteBillionaire(bilionaireiD);
 	}
 
 	@Override
 	public int createBillionaires(Billionaires billionaires) {
-		System.out.println("SEERVVICE SAVE---");
 		return billionairesDaoImpl.createBillionaires(billionaires);
 	}
 
 	@Override
 	public boolean updateBillionaire(Billionaires billionaires) {
-		System.out.println("SEERVVICE update---");
 		return billionairesDaoImpl.updateBillionaire(billionaires);
 	}
-	 
-
-
 
 }
